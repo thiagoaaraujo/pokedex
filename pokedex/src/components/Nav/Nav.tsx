bc2deb0683e0c757606544ecc43cac43dfe56db8
Thiago Aparecido Araújo <thiagoaaraujo@unipam.edu.br>
2023-10-25 17:02:12 -0300

    NavBar vermelha

--- pokedex/src/components/Nav/Nav.tsx
+++ pokedex/src/components/Nav/Nav.tsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import styles from './Nav.module.css';
-import * as data from './links.json';
 import logo from '../../assets/logoPokemon.png';
 
-const linksString = JSON.stringify(data);
-const links = JSON.parse(linksString).links;
 
-type Link = {
-    label: string;
-    href: string;
-};
-
-const Links: React.FC<{ links: Link[] }> = ({ links }) => {
-    return (
-        <div className={styles['links-container']}>
-            {links.map((link: Link) => {
-                return (
-                    <div key={link.href} className={styles['link']}>
-                        <a href={link.href}>
-                            {link.label}
-                        </a>
-                    </div>
-                )
-            })}
-        </div>
-
-    )
-};
 
 const Nav: React.FC<{}> = () => {
     return (
@@ -34,7 +10,6 @@ const Nav: React.FC<{}> = () => {
             <div className={styles['logo-container']}>
                 <img src = { logo } />
             </div>
-            <Links links={links} />
         </nav>
     )
 }
