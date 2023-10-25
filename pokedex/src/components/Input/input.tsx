6dbbc13cdeb64c4305c36e7f2a6a1c7fc5187700
Thiago Aparecido Araújo <thiagoaaraujo@unipam.edu.br>
2023-10-24 23:31:49 -0300

    NavBar parcial

---
+++ pokedex/src/components/Input/input.tsx
@@ -0,0 +1,11 @@
+
+
+export default function InputPage() {
+    return(
+        <>
+            <h2>Teste</h2>
+            <input type="text" />
+            <button type="submit">Enviar</button>
+        </>
+    )
+}
