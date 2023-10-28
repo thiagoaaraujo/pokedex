d6c0602a83b18de3ce732f76e89dc20b8149ebe8
Guilherme Caixeta <guilherme.caixeta@uaigraos.com.br>
2023-10-28 07:39:58 -0300

    gggg

--- pokedex/src/components/Input/input.tsx
+++ pokedex/src/components/Input/input.tsx
@@ -1,20 +1,46 @@
 import styles from './input.module.css';
-import pokemon from '../../assets/13.png';
+import { useState, useEffect } from 'react';
+import { getRandomPokemon } from '../../services/htttp.config';
 
 export default function InputPage() {
+
+    const [pokemon, setPokemon] = useState<any>()
+    const [input, setInput] = useState<any>()
+    const [resp, setResp] = useState<string | null>(null)
+
+    useEffect(() => {
+        getPokemon()
+    }, [])
+
+
+    function getPokemon() {
+        getRandomPokemon().then(setPokemon)
+        setResp(null)
+    } 
+
+    function checkPokemon() {
+        if (input?.toLowerCase() === pokemon?.name) {
+            setResp('Parabéns você acertou!')
+        } else{
+            setResp('Que pena...')
+        }
+    }
+
+
     return (
         <>
             <div className={styles['container']}>
-                <div className={styles['container-image']}>
-                    <img src={pokemon} className={styles['image']} />
-                    <h1>Acertou!</h1>
-                    <h1>Errou!</h1>
+            
+                <div className={styles['container-image']} style={{filter: `brightness(${resp ? '100' : '0'}%)`}}>
+                    <h1>{resp ? `É o ${pokemon?.name[0].toUpperCase() + pokemon?.name.substring(1)}` : ''}</h1>
+                    <img src={pokemon?.sprite} className={styles['image']} alt='Pokemon'/>
+                    <h1>{resp ?? ''}</h1>
                 </div>
                 <div className={styles['container-input']}>
                     <h2 className={styles['title']}>Quem é esse pokémon?</h2>
-                    <input className={styles['input']} type="text" />
-                    <button className={styles['button']} type="submit">Enviar</button>
-                    <button className={styles['buttonReset']} type="submit">Reiniciar</button>
+                    <input className={styles['input']} onChange={(e) => setInput(e.target.value)} type="text" />
+                    <button className={styles['button']} onClick={() => checkPokemon()} type="submit">Enviar</button>
+                    <button className={styles['buttonReset']} onClick={() => getPokemon()}  type="submit">Reiniciar</button>
                 </div>
             </div>
         </>
