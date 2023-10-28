564dd36cbf7057c971a862f39c99ed569a9eab1d
Thiago Aparecido Araújo <thiagoaaraujo@unipam.edu.br>
2023-10-27 22:37:35 -0300

    Imagem do Pokémon e input/botão

--- pokedex/src/components/Input/input.tsx
+++ pokedex/src/components/Input/input.tsx
@@ -1,11 +1,19 @@
-import styles from './input.module.css'
+import styles from './input.module.css';
+import pokemon from '../../assets/13.png';
 
 export default function InputPage() {
-    return(
+    return (
         <>
-            <h2 className={styles['title']}>Pokémon</h2>
-            <input className={styles['input']} type="text" />
-            <button className={styles['button']} type="submit">Enviar</button>
+            <div className={styles['container']}>
+                <div className={styles['container-image']}>
+                    <img src={pokemon} className={styles['image']} alt='Pokémon' />
+                </div>
+                <div className={styles['container-input']}>
+                    <h2 className={styles['title']}>Quem é esse pokemon?</h2>
+                    <input className={styles['input']} type="text" />
+                    <button className={styles['button']} type="submit">Enviar</button>
+                </div>
+            </div>
         </>
     )
 }
